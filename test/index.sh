a3e35063a4d6853562bd3807258b0d3ea9167941
zhongzhi107 <zhong.zhi@163.com>
2019-01-29 17:21:02 +0800

    Update index.sh

--- test/index.sh
+++ test/index.sh
@@ -7,8 +7,7 @@ cp ../package.json node_modules/dace-plugin-less
 ./node_modules/.bin/dace build
 
 # 检查编译结果
-line=`cat prd/css/index.css|grep '.a .b'|wc -l`
-
-if [ $line -eq 0 ]; then
+lineCount=`cat prd/css/index.css|grep '.a .b'|wc -l`
+if [ $lineCount -eq 0 ]; then
   exit 1;
 fi
